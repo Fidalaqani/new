e279dd703d6eb6b8761094d7e48e1fdff45800b1
fida ali <fidaalibaig2002@gmail.com>
2021-12-27 13:49:56 +0500

    third commit in file1.py

--- file1.py
+++ file1.py
@@ -3,4 +3,7 @@ print("you entered your name ")
 print("enter your age ")
 print("you entered your age ")
 print("enter your id ")
+print("enter your reg_no ")
+print("enter your course name ")
+print("enter your teaher name ")
 
